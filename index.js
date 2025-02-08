5d96d856b5529c290904ececf6128b80dcaee5fa
gaurav01singh <gauravsingh02195@gmail.com>
2025-02-08 16:49:28 +0530

    updated

--- index.js
+++ index.js
@@ -3,31 +3,28 @@ import connectDB from "../config/db.js";
 import dotenv from "dotenv";
 import userRoutes from "../routes/userRoutes.js"; 
 import blogRoutes from "../routes/blogRoutes.js"; 
-import categoryRoutes from "../routes/categoryRoutes.js"
+import categoryRoutes from "../routes/categoryRoutes.js";
 import cors from "cors";
 
+// Load environment variables
+dotenv.config();
 
-
-const Port = process.env.PORT || 5000;
+// Connect to database
 connectDB();
 
-dotenv.config();
 const app = express();
-
-app.use(cors()); 
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit:100000}));
-
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 100000 }));
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("hello from Vercel!");
 });
 
+// API routes
 app.use("/api/user", userRoutes);
 app.use("/api/blog", blogRoutes);
 app.use("/api/category", categoryRoutes);
 
-app.listen(Port, () => {
-  console.log(`App running on http://localhost:${Port}`);
-});
+// Export the app for Vercel
+export default app;
