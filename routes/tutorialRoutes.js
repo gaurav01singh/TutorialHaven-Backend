322e60a09e91dd7f83708dc88f93f354d52b7f8b
gaurav01singh <gauravsingh02195@gmail.com>
2025-04-08 13:12:30 +0530

    make api fast

--- routes/tutorialRoutes.js
+++ routes/tutorialRoutes.js
@@ -24,7 +24,7 @@ router.post("/create", authenticateToken, isAdmin, async (req, res) => {
 // ✅ Get all tutorials
 router.get("/all", async (req, res) => {
   try {
-    const tutorials = await Tutorial.find().populate("createdBy", "name email").populate("category", "name");
+    const tutorials = await Tutorial.find({},'title createdBy category').populate("createdBy", "name email").populate("category", "name");
     res.status(200).json(tutorials);
   } catch (error) {
     res.status(500).json({ message: "Error fetching tutorials", error: error.message });
@@ -45,7 +45,7 @@ router.get("/:slug", async (req, res) => {
 // ✅ Get tutorials by subcategory
 router.get("/subcategory/:subcategoryId", async (req, res) => {
   try {
-    const tutorials = await Tutorial.find({ subcategory: req.params.subcategoryId }).populate("category", "name");
+    const tutorials = await Tutorial.find({ subcategory: req.params.subcategoryId },'title createdBy category').populate("category", "name");
     res.json(tutorials);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -53,7 +53,7 @@ router.get("/subcategory/:subcategoryId", async (req, res) => {
 });
 router.get("/category/:categoryId", async (req, res) => {
   try {
-    const tutorials = await Tutorial.find({ category: req.params.categoryId });
+    const tutorials = await Tutorial.find({ category: req.params.categoryId },'title createdBy category');
     res.json(tutorials);
   } catch (error) {
     res.status(500).json({ error: error.message });
